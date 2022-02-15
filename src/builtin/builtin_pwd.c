79e8561d7bc368d2b9e1c73c04f05c476b9ea79e
Anna-saray Rohmann <arohmann@1-C-1.42heilbronn.de>
2022-02-15 14:39:09 +0100

    pwd working

--- src/builtin/builtin_pwd.c
+++ src/builtin/builtin_pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_pwd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
+/*   By: arohmann <arohmann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 22:33:57 by annarohmnn        #+#    #+#             */
-/*   Updated: 2022/02/13 21:05:01 by amorcill         ###   ########.fr       */
+/*   Updated: 2022/02/15 14:36:10 by arohmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,14 @@
 void	exec_pwd(t_info *ms, t_program *pgm)
 {
 	char	*cwd;
-	char	*p;
-	t_env	*pwd;
 
 	(void)ms;
 	(void)pgm;
-	p = ft_strdup("PWD");
 	cwd = getcwd(NULL, 0);
-	pwd = ms_find_env_var(ms, &p);
-	free(pwd->content);
-	if (pwd != NULL)
-		pwd->content = cwd;
 	if (cwd == NULL)
 		error_exit("error", " No such file or directory");
 	ft_putstr_fd(cwd, STDOUT_FILENO);
 	ft_putstr_fd("\n", STDOUT_FILENO);
 	if (cwd)
 		free(cwd);
-	free(p);
 }
