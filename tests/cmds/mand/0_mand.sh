1612ee4e328011f5454e8227a4a76ba7c269f079
Anna Rohmann <anna.sy.roh@gmail.com>
2022-02-17 11:00:11 +0100

    new tester file for important cases

--- tests/cmds/mand/0_mand.sh
+++ tests/cmds/mand/0_mand.sh
@@ -1,3 +1,39 @@
 # ############################################################################ #
 #   MANDATORY TESTS                                                            #
 # ############################################################################ #
+
+/bin/echo
+
+/bin/echo hello
+
+/bin/echo hello$USER
+
+/bin/echo hello $USER
+
+echo $USER$PATH
+
+/bin/echo "hello"
+
+/bin/echo 'hello'
+
+echo hello | hostname
+
+unset PATH
+/bin/ls
+
+cat | ls
+
+<< end <Makefile cat
+
+<< end cat
+
+echo hello | exit | hostname
+
+echo hello | wggwd | hostname
+
+echo hello || | uao
+
+echo hello || hostname
+
+cd
+
